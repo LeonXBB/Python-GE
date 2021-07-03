62af4e8bd16d9c1ce7affb55c779746875dbb088
LeonXBB <lbarenbojm@gmail.com>
2021-07-03 17:50:02 +0300

    finished enging starting screen fix

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from py.Engine import Engine as Engine
-from kivy.clock import Clock
 
 appEngine = Engine()
 appEngine.start()
--- py/Engine.py
+++ py/Engine.py
@@ -1,34 +1,37 @@
-from re import I
 from kivy.app import App
 
-from kivy.uix.screenmanager import ScreenManager
+from kivy.clock import Clock
+from functools import partial
+
+from kivy.uix.screenmanager import ScreenManager, NoTransition
 
 from gfx.screens.LoadingScreen import LoadingScreen as LoadingScreen
 from gfx.screens.StartingScreen import StartingScreen as StartingScreen
 
-import time
-
 class Engine(App):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.screenManager = ScreenManager(transition=NoTransition())
+        self.screenManager.add_widget(LoadingScreen())
+
+        self.clock = Clock
 
     def build(self, **kwargs):
 
         super().__init__(**kwargs)
-        self.screenManager = ScreenManager()
-        self.screenManager.add_widget(LoadingScreen())
         return self.screenManager
 
     def start(self):
 
-        def process_starting_screen(engine):
+        def process_starting_screen(self, dt):
             
-            engine.screenManager.add_widget(StartingScreen())
-            engine.screenManager.current = 'Starting Screen'
+            self.screenManager.add_widget(StartingScreen())
+            self.screenManager.current = 'Starting Screen'
 
+        self.clock.schedule_once(partial(process_starting_screen, self), -1)
         self.run()
-        process_starting_screen(self)
-        self.screenManager.current = 'Starting Screen'
-        time.sleep(1000000)
-
 
     def change_settings(self):
         pass

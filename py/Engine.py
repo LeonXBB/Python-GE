d706fbd2a5514a9a55938aa15137e04b28454fbd
LeonXBB <lbarenbojm@gmail.com>
2021-07-06 23:05:35 +0300

    Add files via upload
    fixed some errors related to uploading big files
    finished working on settings (read-only for now) and JSON files
    almost finished audio-part of the engine

--- py/Engine.py
+++ py/Engine.py
@@ -8,6 +8,13 @@ from kivy.uix.screenmanager import ScreenManager, NoTransition
 from gfx.screens.LoadingScreen import LoadingScreen as LoadingScreen
 from gfx.screens.StartingScreen import StartingScreen as StartingScreen
 
+from kivy.core.audio import SoundLoader
+
+import time
+import os 
+import random
+import threading
+
 class Engine(App):
 
     def __init__(self, **kwargs):
@@ -29,8 +36,12 @@ class Engine(App):
             
             self.screenManager.add_widget(StartingScreen())
             self.screenManager.current = 'Starting Screen'
-
+        
         self.clock.schedule_once(partial(process_starting_screen, self), -1)
+        
+        audioThread = threading.Timer(0.5, self.manage_audio)
+        audioThread.start()
+        
         self.run()
 
     def change_settings(self):
@@ -39,8 +50,27 @@ class Engine(App):
     def load_settigns(self):
         pass
 
-    def play_audio(self):
-        pass
+    def manage_audio(self):
+
+        def get_order():
+
+            for root, dirs, files in os.walk('audio'):
+                audioFilesNumber = len(files)
+            
+            audioNumbersList = list(range(1, audioFilesNumber+1))
+            random.shuffle(audioNumbersList)
+
+            return audioNumbersList
+
+        def play_audio(index):
+            
+            audioFile = SoundLoader.load('audio/' + str(index) + '.wav')
+            audioFile.play()
+            time.sleep(audioFile.length)
+
+        order = get_order()
+        for index in order:
+            play_audio(index)
 
     def time_tick(self):
         pass

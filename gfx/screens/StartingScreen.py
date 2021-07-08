8509b05b31b9b5707d4edcd1c401aa34fe657139
LeonXBB <lbarenbojm@gmail.com>
2021-07-08 03:47:52 +0300

    started StartingScreen

--- gfx/screens/StartingScreen.py
+++ gfx/screens/StartingScreen.py
@@ -1,3 +1,4 @@
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import Screen
 
 from kivy.uix.label import Label
@@ -7,4 +8,11 @@ class StartingScreen(Screen):
     def __init__(self, **kw):
         super().__init__(**kw)
         self.name = 'Starting Screen'
-        self.add_widget(Label(text="THIS IS A STARTING SCREEN"))
+
+        self.layout = FloatLayout()
+
+        self.layout.appNameLabel = Label(text='AIRWORDS', pos=(40,115))
+
+        self.layout.add_widget(self.layout.appNameLabel)
+
+        self.add_widget(self.layout)

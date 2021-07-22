a085aaafa8d24d0600fda74ea0a3bcbec7a62482
LeonXBB <barenbojm@gmail.com>
2021-07-22 11:23:19 +0300

    moved a bunch of stuff around once again

--- engine/gfx/py/3d/Model.py
+++ engine/gfx/py/3d/Model.py
@@ -6,5 +6,5 @@ class Model(Widget):
         super().__init__(**kwargs)
 
         if not hasattr(self, "texture"): self.texture = "default"
-
-
+        if not hasattr(self, "joints"): self.joints = []        
+        if not hasattr(self, "poligons"): self.poligons = []
